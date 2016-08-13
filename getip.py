bd058e162bf6f62c6192a3b4ccc89afe94016987
Manwholikespie <rdominguez@me.com>
2016-08-13 18:48:03 -0500

    Update getip.py
    Added a function for finding your private IP address (at least on OS X). Next up, Linux, and then Windows.

--- getip.py
+++ getip.py
@@ -1,12 +1,33 @@
 import urllib2
+import os
+import subprocess
 
-def get_ip():
+def get_pub_ip():
+    print("Finding public IP. Consulting amazonaws...")
     try:
         data = urllib2.urlopen("http://checkip.amazonaws.com")
         ip = data.read().replace("\n","")
         return ip
     except:
-        print("There was an error.")
+        print("There was an error, most likely with your internet or urllib2.")
         return urllib2.URLError
 
-test()
+def get_priv_ip():
+    print("Finding private IP. Checking network interfaces...")
+
+    # en0
+    try:
+        en0 = subprocess.check_output("ipconfig getifaddr en0", shell=True).replace("\n","")
+        print("Found IP on en0 (Ethernet): " + en0)
+        return en0
+    except:
+        pass
+
+    # en1
+    try:
+        en1 = subprocess.check_output("ipconfig getifaddr en1", shell=True).replace("\n","")
+        print("Found IP on en1 (Wireless): " + en1)
+        return en1
+    except:
+        print("No private addresses found. Consult ifconfig.")
+        return 1
