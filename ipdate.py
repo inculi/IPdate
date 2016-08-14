e920cbb836d77186239987ebb174290ac9fdbb7e
James McGregor <jay911144@gmail.com>
2016-08-14 01:30:14 -0500

    re-added print statements

--- ipdate.py
+++ ipdate.py
@@ -5,6 +5,7 @@ import platform
 import smtplib
 
 def get_pub_ip():
+    print("Finding public IP. Consulting amazonaws...")
     try:
         data = urllib2.urlopen("http://checkip.amazonaws.com")
         ip = data.read().replace("\n","")
@@ -14,12 +15,14 @@ def get_pub_ip():
         return urllib2.URLError
 
 def get_priv_ip():
+    print("Finding private IP. Checking network interfaces...")
     if platform.system() == 'Darwin': # OS X
         interfaces = ['en0','en1'] # en0 is ethernet, en1 is wireless.
         for interface in interfaces:
             try:
                 command = str("ipconfig getifaddr "+interface)
                 output = subprocess.check_output(command, shell=True).replace("\n","")
+                print("Found IP on "+interface+": " + output)
                 return output
             except:
                 pass
@@ -33,10 +36,12 @@ def get_priv_ip():
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(('google.com', 0))
         priv_ip = s.getsockname()[0]
+        print("Found IP: " + priv_ip)
         return priv_ip
 
     elif platform.system() == 'Linux': # Linux
         output = subprocess.check_output("hostname -I", shell=True).replace("\n","")
+        print("Found IP: " + output)
         return output
 
     else:
@@ -64,7 +69,9 @@ if platform.system() == 'Darwin' or platform.system() == 'Linux':
     msgFile.write(msg)
     msgFile.close()
 
+    print("Sending email to "+email+". Make sure to check your Junk folder.")
     os.system("sendmail " + email + " < msg.txt")
+    print("Message sent.")
 elif platform.system() == 'Windows':
     if not os.path.isfile(os.path.expanduser('~') + "/email.dat"):
         print "No email credentials found. Please enter them now:"
@@ -83,6 +90,7 @@ elif platform.system() == 'Windows':
         username = content[0]
         password = content[1]
         email = content[2]
+        print content
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(username, password)
